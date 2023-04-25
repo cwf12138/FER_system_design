7d8ef6dc41f26928a63176353b6f907ed418acf6
cwf12138 <2537319936@qq.com>
2023-04-25 20:10:08 +0800

    Finally, I can pause playback and close the video stream, and center the display, happy

--- src/recognition_camera.py
+++ src/recognition_camera.py
@@ -15,8 +15,8 @@ from blazeface import blaze_detect
 
 
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel,QDesktopWidget,QHBoxLayout, QVBoxLayout, QPushButton, QWidget
+from PyQt5.QtCore import QObject, pyqtSignal,Qt, QTimer
 
 
 
@@ -65,32 +65,131 @@ def generate_faces(face_img, img_size=48):
     return resized_images
 
 
-
-class App(QMainWindow):
-    def __init__(self,video_capture, parent=None):
+#QMainWindow
+class App(QWidget):
+    def __init__(self,model, parent=None):
         super().__init__()
         self.title = 'Face Recognition'
-        self.left = 100
-        self.top = 100
-        self.width = 640
-        self.height = 480
-        self.video_capture = video_capture
+        self.left = 0
+        self.top = 0
+        self.width = 800
+        self.height = 600
+        self.model=model
+        #self.video_capture = video_capture
         self.initUI()
+
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-
+        # 定义一个定时器
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.predict_expression_test)   
+        # 创建一个标签用于显示视频流
+        self.label_video = QLabel(self)
+        self.label_video.setAlignment(Qt.AlignCenter)
         # 创建标签，用于显示视频流
-        self.label = QLabel(self)
-        self.label.setGeometry(10, 10, 640, 480)
+        #self.label = QLabel(self)
+        #self.label.setGeometry(10, 10, 640, 480)
+        # 创建一个按钮用于开始/暂停视频流的播放
+        self.btn_play_pause = QPushButton("Play", self)
+        self.btn_play_pause.clicked.connect(self.play_pause_video)
+        # 创建一个按钮用于停止视频流的播放
+        self.btn_stop = QPushButton("Stop", self)
+        self.btn_stop.clicked.connect(self.stop_video)
+        # 创建一个水平布局
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.btn_play_pause)
+        hbox.addWidget(self.btn_stop)
+
+        # 创建一个垂直布局，将标签和水平布局添加进去
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.label_video)
+        vbox.addLayout(hbox)
+
+        # 将垂直布局应用于主窗口
+        self.setLayout(vbox)
+
+        # 打开视频流
+        #self.cap = cv2.VideoCapture(0)
+        self.capture = cv2.VideoCapture(0)  # 指定0号摄像头
+        self.timer.start(30)
+        #居中显示
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
         self.show()
+    def predict_expression_test(self):   #测试暂停键和关闭键是否成功
+
+        #model = load_model()
+
+        border_color = (0, 0, 0)  # 黑框框
+        font_color = (255, 255, 255)  # 白字字
+        #capture = cv2.VideoCapture(0)  # 指定0号摄像头
+        #app = QApplication(sys.argv)
+        #ex = App(capture)
+        if filename:
+            self.capture = cv2.VideoCapture(filename)
+        
+        _, frame = self.capture.read()  # 读取一帧视频，返回是否到达视频结尾的布尔值和这一帧的图像
+            #frame = cv2.cvtColor(cv2.resize(frame, (800, 600)), cv2.COLOR_BGR2RGB)
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 灰度化
+            # cascade = cv2.CascadeClassifier('./dataset/params/haarcascade_frontalface_alt.xml')  # 检测人脸
+            # # 利用分类器识别出哪个区域为人脸
+            # faces = cascade.detectMultiScale(frame_gray, scaleFactor=1.1, minNeighbors=1, minSize=(120, 120))
+        faces = blaze_detect(frame)
+        if faces is not None and len(faces) > 0:
+            for (x, y, w, h) in faces:
+                face = frame_gray[y: y + h, x: x + w]  # 脸部图片
+                faces = generate_faces(face)
+                results = self.model.predict(faces)
+                result_sum = np.sum(results, axis=0).reshape(-1) #这里也可以加一个概率统计 ，frame 和 possibilities
+                label_index = np.argmax(result_sum, axis=0)
+                emotion = index2emotion(label_index)  #*这里可以注意一下
+                cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), border_color, thickness=2)
+                frame = cv2_img_add_text(frame, emotion, x+30, y+30, font_color, 20)   #*这里也是
+                    # puttext中文显示问题
+                    # cv2.putText(frame, emotion, (x + 30, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, font_color, 4)
+        qImg = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+            # 将 QImage 对象转换为 QPixmap 对象
+        pixmap = QPixmap.fromImage(qImg)
+            # 在标签上显示图像
+        self.label_video.setPixmap(pixmap)
+        #key = cv2.waitKey(30)  # 等待30ms，返回ASCII码
+                                            
+            # 如果输入esc则退出循环
+        #capture.release()  # 释放摄像头
+        #cv2.destroyAllWindows()  # 销毁窗口
+        #sys.exit(app.exec_())
+        #return frame
+    def play_pause_video(self):
+        """开始/暂停视频流的播放"""
+        if not self.timer.isActive():
+            self.timer.start(30)
+            self.btn_play_pause.setText("Pause")
+        else:
+            self.timer.stop()
+            self.btn_play_pause.setText("Play")
+
+    def stop_video(self):
+        """停止视频流的播放"""
+        self.timer.stop()
+        self.btn_play_pause.setText("Play")
+        self.label_video.clear()
+
     def closeEvent(self, event):
         # 触发信号
         # 关闭窗口
-        self.video_capture.release()
+        self.capture.release()
         #cv2.destroyAllWindows()
         event.accept()
+
+
+
+
+    
 
 def predict_expression():
     """
@@ -110,7 +209,8 @@ def predict_expression():
 
     while True:
         _, frame = capture.read()  # 读取一帧视频，返回是否到达视频结尾的布尔值和这一帧的图像
-        frame = cv2.cvtColor(cv2.resize(frame, (800, 600)), cv2.COLOR_BGR2RGB)
+        #frame = cv2.cvtColor(cv2.resize(frame, (800, 600)), cv2.COLOR_BGR2RGB)
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 灰度化
         # cascade = cv2.CascadeClassifier('./dataset/params/haarcascade_frontalface_alt.xml')  # 检测人脸
         # # 利用分类器识别出哪个区域为人脸
@@ -133,10 +233,10 @@ def predict_expression():
         # 将 QImage 对象转换为 QPixmap 对象
         pixmap = QPixmap.fromImage(qImg)
         # 在标签上显示图像
-        ex.label.setPixmap(pixmap)
+        ex.label_video.setPixmap(pixmap)
 
         # 每 25 毫秒刷新一次界面
-        QApplication.processEvents()
+        #QApplication.processEvents()
         #cv2.waitKey(25)
         #cv2.imshow("expression recognition(press esc to exit)", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # 利用人眼假象，这里是展示环节
         #frame=cv2.cvtColor(frame,cv2.COLOR_HSV2BGR)
@@ -150,7 +250,12 @@ def predict_expression():
     #cv2.destroyAllWindows()  # 销毁窗口
     #sys.exit(app.exec_())
     #return frame
-
+    app.exec_()
 
 if __name__ == '__main__':
-    predict_expression()
+    #predict_expression()
+    app = QApplication(sys.argv)
+    model = load_model()
+    ex = App(model)
+    ex.show()
+    app.exec_()
