e57782436b5899fcb84ed5fc1dd4f391a3ea38d1
cwf12138 <2537319936@qq.com>
2023-05-03 10:49:13 +0800

    The histogram shows that it has been completed and is still to be added to the system

--- src/qtchar.py
+++ src/qtchar.py
@@ -1,33 +1,52 @@
-from PyQt5.QtChart import QChart, QChartView, QBarSet, QBarSeries
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtChart import QChart, QChartView, QBarSeries, QBarSet, QBarCategoryAxis, QValueAxis
+from PyQt5.QtCore import Qt, QRectF
+from PyQt5.QtGui import QPainter
 
-app = QApplication([])
-window = QMainWindow()
+class BarChart(QMainWindow):
+    def __init__(self, x, y):
+        super().__init__()
 
-# 创建数据集
-data = [1, 2, 3, 4, 5]
-bar_set = QBarSet('数据集')
-for value in data:
-    bar_set.append(value)
+        # 创建条形集合并添加数据
+        set0 = QBarSet("Quantity")
+        for value in y:
+            set0.append(value)
 
-# 创建条形图系列
-bar_series = QBarSeries()
-bar_series.append(bar_set)
+        # 将条形集合添加到条形系列中
+        series = QBarSeries()
+        series.append(set0)
 
-# 创建图表并添加系列
-chart = QChart()
-chart.addSeries(bar_series)
+        # 创建水平类别轴并设置类别
+        axisX = QBarCategoryAxis()
+        axisX.append(x)
 
-# 设置图表的标题和横坐标标签
-chart.setTitle('条形图')
-chart.setAnimationOptions(QChart.SeriesAnimations)
-chart.createDefaultAxes()
-chart.axisX().setTitleText('横坐标')
-chart.axisY().setTitleText('纵坐标')
+        # 创建数值轴并设置范围
+        axisY = QValueAxis()
+        axisY.setRange(0, max(y)*1.2)
 
-# 创建图表视图并设置图表视图为主窗口的中心部分
-chart_view = QChartView(chart)
-window.setCentralWidget(chart_view)
-window.show()
+        # 将类别轴和数值轴添加到图表中
+        chart = QChart()
+        chart.addSeries(series)
+        chart.setTitle("Emotion Distribution")
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+        chart.addAxis(axisX, Qt.AlignBottom)
+        chart.addAxis(axisY, Qt.AlignLeft)
+        series.attachAxis(axisX)
+        series.attachAxis(axisY)
+        chartView = QChartView(chart)
+        chartView.setRenderHint(QPainter.Antialiasing)
 
-app.exec_()
+        # 设置y轴坐标数值显示
+        chart.axisY().setLabelFormat("%d")
+        
+        # 将图表视图设置为主窗口的中央部件
+        self.setCentralWidget(chartView)
+
+
+if __name__ == "__main__":
+    x = ['anger', 'disgust', 'fear', 'happy', 'sad', 'surprised', 'neutral', 'contempt']
+    y = [10, 5, 7, 15, 3, 8, 20, 2]
+    app = QApplication([])
+    window = BarChart(x, y)
+    window.show()
+    app.exec_()
